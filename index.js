1d44b60988f94df770f8f51efef68cc95010c891
Anakorn Kyavatanakij <anakornk@gmail.com>
2018-11-30 19:22:00 +0800

    addstorybook

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App/Containers/App';
 import {name as appName} from './app.json';
+import StorybookUI from './storybook';
 
+let app = (false) ? StorybookUI : App
 AppRegistry.registerComponent(appName, () => App);

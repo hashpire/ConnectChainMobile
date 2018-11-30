fdb2c67eac11ff49daa7bafb905f4438668508fa
Anakorn Kyavatanakij <anakornk@gmail.com>
2018-11-30 11:24:41 +0800

    add react-navigation

---
+++ App/Containers/RootContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppNavigation from '../Navigation/AppNavigation'
+class RootContainer extends React.Component {
+  render() {
+    return (
+      <AppNavigation/>
+    )
+  }
+}
+
+export default RootContainer;

045a48531e892eda99a2ca5f47c832a804e0300b
Anakorn Kyavatanakij <anakornk@gmail.com>
2018-12-04 12:42:41 +0800

    enable redux dev tools by react-native-config

--- App/Redux/index.js
+++ App/Redux/index.js
@@ -1,9 +1,17 @@
 import { createStore, combineReducers } from 'redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
+import Config from 'react-native-config';
 
 const rootReducer = combineReducers({
   hello: require('./HelloRedux').reducer,
 });
 
 // TODO: hydrate state by adding second parameter
-export default createStore(rootReducer, devToolsEnhancer());
+let store;
+if(Config.ENABLE_REDUX_DEVTOOLS == 1){
+  store =  createStore(rootReducer, devToolsEnhancer());
+} else {
+  store = createStore(rootReducer);
+}
+
+export default store;

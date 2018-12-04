21d61bfcd7d17a750400fb5552ed75f78984cef8
Anakorn Kyavatanakij <anakornk@gmail.com>
2018-12-04 12:43:22 +0800

    change env name

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {name as appName} from './app.json';
 import Config from 'react-native-config'
 import StorybookUI from './storybook';
 
-if(Config.SHOW_STORYBOOK == 1) {
+if(Config.ENABLE_STORYBOOK == 1) {
   AppRegistry.registerComponent(appName, () => StorybookUI);
 } else {
   AppRegistry.registerComponent(appName, () => App);
